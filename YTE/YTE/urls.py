b026df708d271150f43ade10a87c3c989396acb0
tihii8898 <87133096+tihii8898@users.noreply.github.com>
2022-04-27 15:28:15 +0700

    reconstruct code

--- YTE/YTE/urls.py
+++ YTE/YTE/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""YTE URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('base.urls')),
+    path('',include('base.urls')),
 ]
