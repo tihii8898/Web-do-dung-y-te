f08661061263adfd6cdd02697dda8e71198cca29
ThaoHienPhan <hien096456789@gmail.com>
2022-05-11 11:35:57 +0700

    aa

--- YTE/base/models.py
+++ YTE/base/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
 class Product(models.Model):
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200,null=False,blank=False)
